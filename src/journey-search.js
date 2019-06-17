c37cffd819104b4a09ee767e931a71c98a72ef58
Conor Haining <conor.haining@gmail.com>
2019-06-17 16:56:13 +0100

    feat: barebones journey searching

--- src/journey-search.js
+++ src/journey-search.js
@@ -1,6 +1,19 @@
-class JourneySearch extends LitElement {
-    render() {
-      return html`<p>template content</p>`;
-    }
+import { html, LitElement } from 'lit-element';
+
+export class JourneySearch extends LitElement {
+  render() {
+    return html`
+      <location-picker>
+        <label for="from">From</label>
+        <input type="text" name="from" />
+      </location-picker>
+      <location-picker>
+        <label for="to">To</label>
+        <input type="text" name="to" />
+      </location-picker>
+      <input type="submit" name="search" value="Search" />
+    `;
   }
-  
+}
+
+customElements.define('journey-search', JourneySearch);
