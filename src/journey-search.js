6749eb2cc8f5d903bbb7bff06a1fd1d074de7a56
Conor Haining <conor.haining@gmail.com>
2019-06-17 21:10:52 +0100

    feat: made location picker similar to the station picker component in Planrr

--- src/journey-search.js
+++ src/journey-search.js
@@ -3,8 +3,8 @@ import { html, LitElement } from 'lit-element';
 export class JourneySearch extends LitElement {
   render() {
     return html`
-      <location-picker></location-picker>
-      <location-picker></location-picker>
+      <location-picker inputlabel="From"></location-picker>
+      <location-picker inputlabel="To"></location-picker>
       <input type="submit" name="search" value="Search" />
     `;
   }
--- src/location-picker.js
+++ src/location-picker.js
@@ -1,11 +1,114 @@
-import { html, LitElement } from 'lit-element';
-import '@mapbox/mapbox-sdk';
+import { html, css, LitElement } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map.js';
+import { MapboxGeocoder } from './services/mapbox-geocoder';
 
 export class LocationPicker extends LitElement {
+  static get properties() {
+    return {
+      showModal: { type: Boolean },
+      inputLabel: { type: String },
+      selectedLocation: { type: Object, reflect: true },
+      locations: { type: Object },
+    };
+  }
+
+  toggleWindow() {
+    if (this.showModal) {
+      this.showModal = false;
+    } else {
+      this.showModal = true;
+    }
+  }
+
+  selectStation(e) {
+    this.toggleWindow();
+    const station = JSON.parse(e.target.getAttribute('data-station'));
+    this.shadowRoot.querySelector('input.formField').value = station.name;
+    this.selectedStation = station;
+  }
+
+  getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.stations = this._stationInfo.findClosest(position.coords);
+    });
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+      label {
+        float: left;
+        padding-top: 9px;
+      }
+      input.formField {
+        float: right;
+        height: 30px;
+        width: 230px;
+      }
+
+      .modal {
+        position: absolute;
+        background: pink;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        bottom: 0;
+        transform: translateY(100%);
+        z-index: 2;
+        transition: 0.5s transform;
+      }
+      .modal.visible {
+        transform: translateY(0);
+      }
+
+      .modal ul {
+        margin: 30px auto;
+        max-width: 300px;
+        padding: 0;
+      }
+      .modal ul li {
+        padding: 10px 5px;
+        list-style: none;
+        border-top: 1px solid black;
+      }
+      .modal ul li:last-of-type {
+        border-bottom: 1px solid black;
+      }
+    `;
+  }
+
+  handleInput(e) {
+    const query = e.target.value;
+    this.shadowRoot.querySelector('.modal-search').value = query;
+
+    MapboxGeocoder.findCoordinates(query);
+  }
+
   render() {
     return html`
-      <label for="from">From</label>
-      <input type="text" name="from" class="from" />
+      <label for="${this.inputLabel.toLowerCase()}">${this.inputLabel}</label>
+      <input
+        type="text"
+        name="${this.inputLabel.toLowerCase()}"
+        id="${this.inputLabel.toLowerCase()}"
+        class="formField"
+        required
+        @input="${this.textSearch}"
+        @focus="${this.toggleWindow}"
+      />
+      <div class="${classMap({ modal: true, visible: this.showModal })}">
+        <header>
+          <button @click="${this.toggleWindow}">X</button>
+          <!-- TODO Conditional if supported-->
+          <button @click="${this.getCurrentLocation}">Use Current Location</button>
+        </header>
+        <section>
+          <input type="search" class="modal-search" @input="${this.handleInput}" />
+          <ul></ul>
+        </section>
+      </div>
     `;
   }
 }
