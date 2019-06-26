5335eaf1aac0dca8880b1310c326e4d142a092e2
Conor Haining <conor.haining@gmail.com>
2019-06-26 19:17:03 +0100

    fix: some structure to the form

--- src/journey-search.js
+++ src/journey-search.js
@@ -1,11 +1,31 @@
-import { html, LitElement } from 'lit-element';
+import { html, css, LitElement } from 'lit-element';
 
 export class JourneySearch extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+
+      .location-picker {
+        display: block;
+        width: 100%;
+        height: 50px;
+      }
+    `;
+  }
+
   render() {
     return html`
-      <location-picker inputlabel="From"></location-picker>
-      <location-picker inputlabel="To"></location-picker>
-      <input type="submit" name="search" value="Search" />
+      <form>
+        <div class="location-picker">
+          <location-picker inputlabel="From"></location-picker>
+        </div>
+        <div class="location-picker">
+          <location-picker inputlabel="To"></location-picker>
+        </div>
+        <input type="submit" name="search" value="Search" />
+      </form>
     `;
   }
 }
